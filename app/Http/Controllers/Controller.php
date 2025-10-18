694a2ceced32c7ad40bb7af3a44c5af0b4a2e83a
Tiago Esteves Vieira <tiago@tvieira.net>
2025-10-18 19:16:12 -0300

    fix: Remover trait AuthorizesRequests para corrigir erro 403 na criação de QR codes
    - Remover trait AuthorizesRequests do Controller base
    - Corrigir problema de autorização automática que causava erro 403
    - Permitir criação de QR codes sem autorização explícita
    - Manter validação de dados intacta

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,5 @@ use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use ValidatesRequests;
 }
